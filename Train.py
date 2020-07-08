7fd14d3f55becfda9c4332a29c39476f476b7e4b
ppartha03 <prasannaparthasarathi@Prasannas-MacBook-Pro.local>
2020-07-08 12:04:51 -0400

    Initial Commit

---
+++ Train.py
@@ -0,0 +1,287 @@
+from torchtext import data
+from Code.RNN import RecurrentEncoder, Encoder, AttnDecoder, Decoder
+from Code.Transformer import TransformerModel
+#from DatasetUtils.DataIterator import MultiWoZ, PersonaChat
+import torch.optim as optim
+from Utils.Eval_metric import getBLEU
+from optimizers.optim import cAdam
+#from Utils.optim import GradualWarmupScheduler
+from Utils.TransformerUtils import create_masks
+from EncoderDecoder import EncoderDecoder
+import sys
+import torch
+import torch.nn as nn
+import numpy as np
+import argparse
+import os
+import logging
+import wandb
+import random
+import math
+import csv
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+
+# commandline arguments
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+parser = argparse.ArgumentParser()
+parser.add_argument('--results_path', type=str, default = '.')
+parser.add_argument('--s2s_hidden_size', type=int, default = 256)
+parser.add_argument('--s2s_embedding_size',type=int, default = 128)
+parser.add_argument('--transformer_dropout',type=float, default = 0.2)
+parser.add_argument('--transformer_hidden_dim',type=int, default = 512)
+parser.add_argument('--transformer_embedding_dim',type=int, default = 512)
+parser.add_argument('--transformer_n_layers',type=int, default = 2)
+parser.add_argument('--transformer_n_head',type=int, default = 2)
+parser.add_argument('--model',type=str,default='convnet')
+parser.add_argument('--dataset',type=str,default='mnist')
+parser.add_argument('--batch_size',type=int,default=64)
+parser.add_argument('--seed', type=int, default=100)
+parser.add_argument('--lr', type=float, default=1.0)
+parser.add_argument('--gamma', type=float, default=0.7)
+
+args = parser.parse_args()
+
+np.random.seed(args.seed)
+def train(model, iterator, optimizer, criterion, clip, itos_vocab, itos_context_id):
+    ''' Training loop for the model to train.
+    Args:
+        model: A EncoderDecoder model instance.
+        iterator: A DataIterator to read the data.
+        optimizer: Optimizer for the model.
+        criterion: loss criterion.
+        clip: gradient clip value.
+    Returns:
+        epoch_loss: Average loss of the epoch.
+    '''
+    #  some layers have different behavior during train/and evaluation (like BatchNorm, Dropout) so setting it matters.
+    model.train()
+    # loss
+    epoch_loss = 0
+    id_to_hidden = {}
+    # TODO:  convert the hiddenstate to string and save it as text file
+    for i, batch in enumerate(iterator):
+        if model.data == 'text':
+            src = batch.Context.to(device)
+            trg = batch.Target.to(device)
+
+            optimizer.zero_grad()
+
+            # trg is of shape [sequence_len, batch_size]
+            # output is of shape [sequence_len, batch_size, output_dim]
+            if model.type == 'transformer':
+                src = src.transpose(0,1)
+                trg = trg.transpose(0,1)
+                trg_input = trg[:,:-1]
+                src_mask, trg_mask = create_masks(src, trg_input, pad_idx)
+                output,hidden = model(src, src_mask, trg_input, trg_mask)
+                ys = trg[:,1:].contiguous().view(-1)
+                loss = criterion(output.view(-1, output.size(-1)), ys)
+            else:
+                output,hidden = model(src, trg)
+                loss = criterion(output[1:].view(-1, output.shape[2]), trg[1:].view(-1))
+            # loss function works only 2d logits, 1d targets
+            # so flatten the trg, output tensors. Ignore the <sos> token
+            # trg shape shape should be [(sequence_len - 1) * batch_size]
+            # output shape should be [(sequence_len - 1) * batch_size, output_dim]
+            # for b_id,hidden_state in zip(batch.context_id.squeeze(0),hidden.squeeze(0)):
+            #     str_hidden = [itos_context_id[b_id]]
+            #     str_hidden += [str(x.item()) for x in hidden_state]
+            #     hidden_saver_train.write(','.join(str_hidden)+'\n')
+
+
+            # backward pass
+        elif model.data == 'image':
+            src = batch[0]
+            trg = batch[1]
+            output, hidden = model(src)
+            loss = criterion(output, trg)
+        loss.backward()
+
+        # clip the gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        # update the parameters
+        optimizer.step()
+
+        epoch_loss += loss.item()
+    # return the average loss
+    return epoch_loss / len(iterator)
+
+def evaluate(model, iterator, criterion, itos_vocab, itos_context_id, sample_saver):
+    ''' Evaluation loop for the model to evaluate.
+    Args:
+        model: A Seq2Seq model instance.
+        iterator: A DataIterator to read the data.
+        criterion: loss criterion.
+    Returns:
+        epoch_loss: Average loss of the epoch.
+    '''
+    #  some layers have different behavior during train/and evaluation (like BatchNorm, Dropout) so setting it matters.
+    model.eval()
+    # loss
+    epoch_loss = 0
+    epoch_correct = 0
+    # we don't need to update the model parameters. only forward pass.
+    n_batches = 0
+    with torch.no_grad():
+        for i, batch in enumerate(iterator):
+            n_batches +=1
+            if model.data == 'text':
+                epoch_correct = 'NA'
+                src = batch.Context.to(device)
+                trg = batch.Target.to(device)
+                if model.type == 'transformer':
+                    src = src.transpose(0,1)
+                    trg = trg.transpose(0,1)
+                    trg_input = trg[:,:-1]
+                    src_mask, trg_mask = create_masks(src, trg_input, pad_idx)
+                    output,hidden = model(src, src_mask, trg_input, trg_mask)
+                    ys = trg[:,1:].contiguous().view(-1)
+                    loss = criterion(output.view(-1, output.size(-1)), ys)
+                    output = output.permute(1,0,2)
+                else:
+                    output , hidden = model(src,trg,0)  # turn off the teacher forcing
+                    loss = criterion(output[1:].view(-1, output.shape[2]), trg[1:].view(-1))
+                top1 = output.max(2)[1].squeeze(0)
+                # for b_id,hidden_state in zip(batch.context_id.squeeze(0),hidden.squeeze(0)):
+                #     str_hidden = [itos_context_id[b_id]]
+                #     str_hidden += [str(x.item()) for x in hidden_state]
+                #     hidden_saver_eval.write(','.join(str_hidden)+'\n')
+                for b_index in range(len(batch)):
+                    c = ' '.join([itos_vocab[idx.item()] for idx in batch.Context[:,b_index]])
+                    t = ' '.join([itos_vocab[idx.item()] for idx in batch.Target[:,b_index]])
+                    model_res = ' '.join([itos_vocab[idx.item()] for idx in top1[:,b_index]])
+                    sample_saver.write('Context: '+c +'\n'+'Model_Response: '+ model_res +'\n' +'Target: ' + t +'\n\n')
+                # loss function works only 2d logits, 1d targets
+                # so flatten the trg, output tensors. Ignore the <sos> token
+                # trg shape shape should be [(sequence_len - 1) * batch_size]
+                # output shape should be [(sequence_len - 1) * batch_size, output_dim]
+            elif model.data == 'image':
+                src = batch[0]
+                trg = batch[1]
+                output, hidden = model(src)
+                loss = criterion(output, trg)
+                pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+                correct = pred.eq(target.view_as(pred)).mean()
+
+
+            epoch_loss += loss.item()
+            epoch_correct += correct.item()
+    return epoch_loss / len(iterator), 100. * epoch_correct/ len(n_batches)
+
+
+MAX_LENGTH = 101
+BATCH_SIZE = args.batch_size
+run_id = args.model + "_seed_" + str(args.seed)
+if args.dataset == 'MultiWoZ':
+    train_iterator, valid_iterator, test_iterator, pad_idx, INPUT_DIM, itos_vocab, itos_context_id = MultiWoZ(batch_size = BATCH_SIZE ,max_length = MAX_LENGTH)
+elif args.dataset == 'PersonaChat':
+    train_iterator, valid_iterator, pad_idx, INPUT_DIM, itos_vocab, itos_context_id = PersonaChat(batch_size = BATCH_SIZE, max_length = MAX_LENGTH)
+elif args.dataset == 'mnist':
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
+    dataset1 = datasets.MNIST('../data', train=True, download=True,
+                       transform=transform)
+    dataset2 = datasets.MNIST('../data', train=False,
+                       transform=transform)
+    train_iterator = torch.utils.data.DataLoader(dataset1,**kwargs)
+    valid_iterator = torch.utils.data.DataLoader(dataset2, **kwargs)
+
+N_EPOCHS = 25           # number of epochs
+CLIP = 10               # gradient clip value    # directory name to save the models.
+MODEL_SAVE_PATH = os.path.join('Results', args.dataset,'Model',run_id)
+if not os.path.exists(MODEL_SAVE_PATH):
+    os.makedirs(MODEL_SAVE_PATH)
+SAMPLES_PATH = os.path.join('Results',args.dataset,'Samples',run_id)
+if not os.path.exists(SAMPLES_PATH):
+    os.makedirs(SAMPLES_PATH)
+LOG_FILE_NAME = 'logs.txt'
+logging.basicConfig(filename=os.path.join(MODEL_SAVE_PATH,LOG_FILE_NAME),
+                        filemode='a+',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.INFO)
+OUTPUT_DIM = INPUT_DIM
+ENC_EMB_DIM = args.s2s_embedding_size # encoder embedding size
+DEC_EMB_DIM = args.s2s_embedding_size   # decoder embedding size (can be different from encoder embedding size)
+HID_DIM = args.s2s_hidden_size       # hidden dimension (must be same for encoder & decoder)
+N_LAYERS = 2        # number of rnn layers (must be same for encoder & decoder)
+HRED_N_LAYERS = 2
+ENC_DROPOUT = 0   # encoder dropout
+DEC_DROPOUT = 0   # decoder dropout (can be different from encoder droput)
+
+#TransformerParameters
+
+emsize = args.transformer_embedding_dim # 200 embedding dimension
+nhid = args.transformer_hidden_dim #200 the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = args.transformer_n_layers #2 the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhead = args.transformer_n_head #2 the number of heads in the multiheadattention models
+dropout = args.transformer_dropout # 0.2 the dropout value
+
+# encoder
+if args.model == 'convnet':
+    enc = ConvNetEncoder().to(device)
+    dec = ClassDecoder().to(device)
+    model = EncoderDecoder(enc,dec,data='image')
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+if args.model == 'seq2seq':
+    enc = Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT).to(device)
+    dec = Decoder(DEC_EMB_DIM, OUTPUT_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT).to(device)
+    model = EncoderDecoder(enc, dec, attn = False).to(device)
+    optimizer = optim.Adam(model.parameters())
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+elif args.model == 'hred':
+    enc = RecurrentEncoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT).to(device)
+    dec = AttnDecoder(DEC_EMB_DIM, OUTPUT_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT,MAX_LENGTH).to(device)
+    model = EncoderDecoder(enc, dec, attn = True).to(device)
+    optimizer = optim.Adam(model.parameters())
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+elif args.model == 'seq2seq_attn':
+    enc = Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, HRED_N_LAYERS, ENC_DROPOUT).to(device)
+    dec = AttnDecoder(DEC_EMB_DIM, OUTPUT_DIM, HID_DIM, HRED_N_LAYERS, DEC_DROPOUT,MAX_LENGTH).to(device)
+    model = EncoderDecoder(enc, dec, attn = True).to(device)
+    optimizer = optim.Adam(model.parameters())
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+elif args.model == 'bilstm_attn':
+    enc = Encoder(INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT, bi_directional = True).to(device)
+    dec = AttnDecoder(DEC_EMB_DIM, OUTPUT_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT,MAX_LENGTH).to(device)
+    model = EncoderDecoder(enc, dec, attn = True).to(device)
+    optimizer = optim.Adam(model.parameters())
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+elif args.model == 'transformer':
+    model = TransformerModel(INPUT_DIM, emsize, nhead, nlayers, dropout).to(device).to(device)
+    optimizer = optim.Adam(model.parameters(), lr = 0.001)
+    scheduler = GradualWarmupScheduler(optimizer, multiplier=8, total_epoch=2)
+else:
+    print('Error: Model Not There')
+    sys.exit(0)
+
+# loss function calculates the average loss per token
+# passing the <pad> token to ignore_idx argument, we will ignore loss whenever the target token is <pad>
+criterion = nn.CrossEntropyLoss(ignore_index=pad_idx).to(device)
+
+best_validation_perf = float('-inf')
+
+for epoch in range(N_EPOCHS):
+    sample_saver_eval = None
+    valid_perf = 'NA'
+    if model.data == 'text':
+        sample_saver_eval = open(os.path.join(SAMPLES_PATH,"samples_valid_" +str(epoch) +'.txt'),'w')
+    train_loss = train(model, train_iterator, optimizer, criterion, CLIP, itos_vocab, itos_context_id )
+    valid_loss, valid_perf = evaluate(model, valid_iterator, criterion, itos_vocab, itos_context_id, sample_saver_eval)
+    if sample_saver_eval not None:
+        sample_saver_eval.close()
+        valid_perf = getBLEU(sample_saver_eval.name)
+    logging.info(f'| Epoch: {epoch+1:03} | Train Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f} | Val. Loss: {valid_loss:.3f} | Val. PPL: {math.exp(valid_loss):7.3f} | Val. BLEU: {valid_bleu:7.3f} |')
+    print(f'| Epoch: {epoch+1:03} | Train Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f} | Val. Loss: {valid_loss:.3f} | Val. PPL: {math.exp(valid_loss):7.3f} | Val. BLEU: {valid_bleu:7.3f} |')
+    torch.save(model.state_dict(), os.path.join(MODEL_SAVE_PATH,args.model+'_'+str(epoch)+'.pt'))
+    if valid_perf > best_validation_perf:
+        best_validation_perf = valid_perf
+        torch.save(model.state_dict(), os.path.join(MODEL_SAVE_PATH,args.model+'_best_perf.pt'))
+
+    scheduler.step()
---
+++ optimizers/optim.py
@@ -0,0 +1,298 @@
+import math
+import torch
+from .optimizer import Optimizer, required
+
+
+class SGD(Optimizer):
+    r"""Implements stochastic gradient descent (optionally with momentum).
+    Nesterov momentum is based on the formula from
+    `On the importance of initialization and momentum in deep learning`__.
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate
+        momentum (float, optional): momentum factor (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov (bool, optional): enables Nesterov momentum (default: False)
+    Example:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+    .. note::
+        The implementation of SGD with Momentum/Nesterov subtly differs from
+        Sutskever et. al. and implementations in some other frameworks.
+        Considering the specific case of Momentum, the update can be written as
+        .. math::
+                  v_{t+1} = \mu * v_{t} + g_{t+1} \\
+                  p_{t+1} = p_{t} - lr * v_{t+1}
+        where p, g, v and :math:`\mu` denote the parameters, gradient,
+        velocity, and momentum respectively.
+        This is in contrast to Sutskever et. al. and
+        other frameworks which employ an update of the form
+        .. math::
+             v_{t+1} = \mu * v_{t} + lr * g_{t+1} \\
+             p_{t+1} = p_{t} - v_{t+1}
+        The Nesterov version is analogously modified.
+    """
+
+    def __init__(self, params, lr=required, momentum=0, dampening=0,
+                 weight_decay=0, nesterov=False):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}".format(weight_decay))
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, nesterov=nesterov)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        super(SGD, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(SGD, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('nesterov', False)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if weight_decay != 0:
+                    d_p = d_p.add(weight_decay, p.data)
+                if momentum != 0:
+                    param_state = self.state[p]
+                    if 'momentum_buffer' not in param_state:
+                        buf = param_state['momentum_buffer'] = torch.clone(d_p).detach()
+                    else:
+                        buf = param_state['momentum_buffer']
+                        buf.mul_(momentum).add_(1 - dampening, d_p)
+                    if nesterov:
+                        d_p = d_p.add(momentum, buf)
+                    else:
+                        d_p = buf
+
+                p.data.add_(-group['lr'], d_p)
+
+        return loss
+
+class TopK:
+    '''
+    Maintains topk values.
+    Allows insert, getAgg.
+
+    '''
+    def __init__(self,)
+
+class SGD_C(Optimizer):
+    r"""Implements stochastic gradient descent (optionally with momentum).
+    Nesterov momentum is based on the formula from
+    `On the importance of initialization and momentum in deep learning`__.
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): learning rate
+        momentum (float, optional): momentum factor (default: 0)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        dampening (float, optional): dampening for momentum (default: 0)
+        nesterov (bool, optional): enables Nesterov momentum (default: False)
+    Example:
+        >>> optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+    __ http://www.cs.toronto.edu/%7Ehinton/absps/momentum.pdf
+    .. note::
+        The implementation of SGD with Momentum/Nesterov subtly differs from
+        Sutskever et. al. and implementations in some other frameworks.
+        Considering the specific case of Momentum, the update can be written as
+        .. math::
+                  v_{t+1} = \mu * v_{t} + g_{t+1} \\
+                  p_{t+1} = p_{t} - lr * v_{t+1}
+        where p, g, v and :math:`\mu` denote the parameters, gradient,
+        velocity, and momentum respectively.
+        This is in contrast to Sutskever et. al. and
+        other frameworks which employ an update of the form
+        .. math::
+             v_{t+1} = \mu * v_{t} + lr * g_{t+1} \\
+             p_{t+1} = p_{t} - v_{t+1}
+        The Nesterov version is analogously modified.
+    """
+
+    def __init__(self, params, lr=0.001, momentum=0, dampening=0,
+                 weight_decay=0, nesterov =False, topC=10):
+
+        defaults = dict(lr=lr, momentum=momentum, dampening=dampening,
+                        weight_decay=weight_decay, topC=topC)
+        if nesterov and (momentum <= 0 or dampening != 0):
+            raise ValueError("Nesterov momentum requires a momentum and zero dampening")
+        super(SGD_C, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(SGD_C, self).__setstate__(state)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            dampening = group['dampening']
+            nesterov = group['nesterov']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if weight_decay != 0:
+                    d_p = d_p.add(weight_decay, p.data)
+                if momentum != 0:
+                    param_state = self.state[p]
+                    # Critical Gradients
+                    # x_new = x_old - lr * grad
+                    # x_new = x_old - lr * (momentum * grad_<t + (1-dampening) * grad_t)
+                    # understand how the projection happens col_space or row_space
+                    # CG method:
+                    # x_new = x_old - lr * (momentum * grad_CG + (1-dampening) * grad_t)
+                    # grad_CG <- topk gradients
+                    if 'momentum_buffer' not in param_state:
+                        buf = param_state['momentum_buffer'] = torch.clone(d_p).detach()
+                    else:
+                        buf = param_state['momentum_buffer']
+                        buf.mul_(momentum).add_(1 - dampening, d_p)
+                    if nesterov:
+                        d_p = d_p.add(momentum, buf)
+                    else:
+                        d_p = buf
+
+                p.data.add_(-group['lr'], d_p)
+
+        return loss
+
+class Adam(Optimizer):
+    r"""Implements Adam algorithm.
+    It has been proposed in `Adam: A Method for Stochastic Optimization`_.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        amsgrad (boolean, optional): whether to use the AMSGrad variant of this
+            algorithm from the paper `On the Convergence of Adam and Beyond`_
+            (default: False)
+    .. _Adam\: A Method for Stochastic Optimization:
+        https://arxiv.org/abs/1412.6980
+    .. _On the Convergence of Adam and Beyond:
+        https://openreview.net/forum?id=ryQu7f-RZ
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
+                 weight_decay=0, amsgrad=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay, amsgrad=amsgrad)
+        super(Adam, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super(Adam, self).__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('amsgrad', False)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('Adam does not support sparse gradients, please consider SparseAdam instead')
+                amsgrad = group['amsgrad']
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data, memory_format=torch.preserve_format)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data, memory_format=torch.preserve_format)
+                    if amsgrad:
+                        # Maintains max of all exp. moving avg. of sq. grad. values
+                        state['max_exp_avg_sq'] = torch.zeros_like(p.data, memory_format=torch.preserve_format)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                if amsgrad:
+                    max_exp_avg_sq = state['max_exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                # Decay the first and second moment running average coefficient
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+                if amsgrad:
+                    # Maintains the maximum of all 2nd moment running avg. till now
+                    torch.max(max_exp_avg_sq, exp_avg_sq, out=max_exp_avg_sq)
+                    # Use the max. for normalizing running avg. of gradient
+                    denom = (max_exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+                else:
+                    denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
+
+                step_size = group['lr'] / bias_correction1
+
+                p.data.addcdiv_(-step_size, exp_avg, denom)
+
+        return loss
