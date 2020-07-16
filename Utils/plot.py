b4459fe632ac9616812785762960959f8609895d
Prasanna PARTHASARATHI <pparth2@jp-gpu2>
2020-07-16 10:46:13 -0400

    grqphs

--- Utils/plot.py
+++ Utils/plot.py
@@ -16,23 +16,24 @@ args = parser.parse_args()
 # requires csv with headers alpha values, epochs, meteor
 def createData(dataset, Opts, model): # alpha->exp mapping {1:'1E-1',2:'1E-3'}
     #mapdict = {20:'Baseline', 22: 'BERT', 30: 'fastText', 31: 'GloVe'}#{2:'Baseline', 1:'1E-3',4:'1E0'}#{2:'Baseline', 1: '1E-3', 0: '1E-2',3: '1E-1', 4: '1E0', 5:'1E1', 6:'1E2'}
-    seeds = ['100']#,'101','102']
-    lr = ['0.0003','0.0001']
+    seeds = ['100','101','102','103','104']
+    lr = ['0.01','0.001','0.0001']
     topC = ['3']
     kappa = ['0.99']
-    fieldnames=['Model','Dataset','Optimizer','Epoch','Train Loss','Val. Loss','Val. Accuracy']
+    fieldnames=['Model','Dataset','Optimizer','Seed','Learning rate','Epoch','Train Loss','Val. Loss','Val. Accuracy']
     target = open("results_valid.csv", "w")
     writer = csv.DictWriter(target, fieldnames=fieldnames)
     writer.writerow(dict(zip(fieldnames, fieldnames)))
     for v in Opts:
         print('Now gathering info from ',v)
-        for s,l,c,k in itertools.product(seeds,lr,topC,kappa):
-            if '_C' in v:
-                folder = os.path.join('..','Results',args.dataset, model +'_'+ v, 'Model','seed_'+s+'_LR_'+l+'_topC_'+c+'_kappa_'+k)
-                opt = v+c
-            else:
-                folder = os.path.join('..','Results',args.dataset, model+'_' + v, 'Model','seed_'+s+'_LR_'+l)
-                opt = v
+        for s,l in itertools.product(seeds,lr):
+            print(s,l)
+            #if '_C' in v:
+            #    folder = os.path.join('..','Results',args.dataset, model +'_'+ v, 'Model','seed_'+s+'_LR_'+l+'_topC_'+c+'_kappa_'+k)
+            #    opt = v+c
+            #else:
+            folder = os.path.join('..','Results',args.dataset, model+'_' + v, 'Model','seed_'+s+'_LR_'+l)
+            opt = v
             f = open(os.path.join(folder,'logs.txt'))
             ep = 1
             for line in f:
@@ -41,7 +42,9 @@ def createData(dataset, Opts, model): # alpha->exp mapping {1:'1E-1',2:'1E-3'}
                     writer.writerow(dict([
                     ('Model',model),
                     ('Dataset',args.dataset),
-                    ('Optimizer',opt+l),
+                    ('Optimizer',opt),
+                    ('Seed',s),
+                    ('Learning rate',l),
                     ('Epoch',ep),
                     ('Train Loss',line[2].split()[-1]),
                     ('Val. Loss',line[3].split()[-1]),
@@ -52,21 +55,24 @@ def createData(dataset, Opts, model): # alpha->exp mapping {1:'1E-1',2:'1E-3'}
     target.close()
     return fieldnames
 
-def createGraph(yrange=[0.0,0.5], model = 'model', dataset = 'MNIST',filename = 'results_valid.csv', graphparam = 'METEOR', save_file = 'test'):
+def createGraph(yrange=[0.0,0.5], model = 'model', dataset = 'MNIST',filename = 'results_valid.csv', graphparam = 'METEOR', save_file = 'test',opt = None):
     #plt.ylim(yrange[0],yrange[1])
     if 'oss' in graphparam:
         plt.ylim(yrange[0],yrange[1])
-    sns.lineplot(x = 'Epoch', y =graphparam, hue = 'Optimizer',data = pd.read_csv(filename))
-    plt.title('Perf. of '+model+' on' +dataset + ' dataset')
+    sns.lineplot(x = 'Epoch', y =graphparam, hue = 'Learning rate',data = pd.read_csv(filename))
+    if opt != None:
+        plt.title('Perf. of '+model+' on' +dataset + ' dataset with Optimizer as ' + opt[0])
+    else:
+        plt.title('Perf. of '+model+' on' +dataset + ' dataset')
     plt.savefig(save_file)
     plt.close()
 
 if __name__ == '__main__':
-    Opts = [['SGD_C']]#,('Adam_C','Adam')]#,('SGDM_C','SGDM')]
-    models = ['NeuralNet']#,'LR']
+    Opts = [['SGD']]#,('Adam_C','Adam')]#,('SGDM_C','SGDM')]
+    models = ['NeuralNet','LR']
     for model in models:
         print(model)
         for opt_ in Opts:
             header = createData(args.dataset, model = model, Opts = opt_)
-            for param in header[4:]:
-                createGraph(graphparam = param, model = model, save_file = model+'_'+param + '_'.join(opt_)+'.png')
+            for param in header[6:]:
+                createGraph(graphparam = param, model = model, save_file = model+'_'+param + '_'.join(opt_)+'.png',opt = opt_)
