581a57c835829260b6e3d97431eb6a636723aed6
Pranshu Malviya <pranshumalviya2@gmail.com>
2022-05-18 09:57:30 -0400

    Added efficient GPU-based implementation

--- cgoptimizer/priority_dict.py
+++ cgoptimizer/priority_dict.py
@@ -1,4 +1,4 @@
-from heapq import heapify, heappush, heappop
+from heapq import heapify
 import torch
 from copy import deepcopy
 
@@ -10,6 +10,111 @@ class HeapItem:
 
     def __lt__(self, other):
         return self.p < other.p
+
+
+class TensorList(dict):
+    """List that can be used as a priority queue.
+
+    The 'smallest' method can be used to return the object with lowest
+    priority
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(TensorList, self).__init__(*args, **kwargs)
+        self.aggr_sum = None
+        # self.aggr_sq_sum = None
+        self.smallest = 0
+
+    def getNorms(self):
+        return self._heap_key
+
+    def size(self):
+        return self.curr_k
+
+    def setHyper(self, decay_rate=0.5, K=5, dtype=None):
+        self.k = K
+        self.curr_k = 0
+        self.decay_rate = decay_rate
+        self.dtype = dtype
+
+    def addItem(self, key, val):
+        if self.dtype is not None:
+            val = val.to(dtype=self.dtype)
+        if self.isFull():
+            self.aggr_sum.add_(-self._heap[self.smallest])
+            self._heap_key[self.smallest] = key
+            self._heap[self.smallest] = val
+        else:
+            if self.curr_k==0:
+                self._heap_key = torch.zeros(self.k, device='cuda' if torch.cuda.is_available() else 'cpu', dtype=val.dtype)
+                self._heap = torch.zeros(self.k, *val.shape, device='cuda' if torch.cuda.is_available() else 'cpu', dtype=val.dtype)
+            self._heap_key[self.curr_k] = key
+            self._heap[self.curr_k] = val
+            self.curr_k += 1
+
+        if self.aggr_sum is None:
+            self.aggr_sum = torch.zeros_like(self._heap[0], device='cuda' if torch.cuda.is_available() else 'cpu')
+            # self.aggr_sq_sum = torch.zeros_like(val)
+        self.aggr_sum.add_(val)
+        # self.aggr_sq_sum.addcmul_(val, val)
+
+    def pokeSmallest(self):
+        """Return the lowest priority.
+        Raises IndexError if the object is empty.
+        """
+        self.smallest = torch.argmin(self._heap_key)
+        return self._heap_key[self.smallest]
+
+    def isEmpty(self):
+        return self.curr_k == 0
+
+    def decay(self):
+        self._heap_key = torch.mul(self._heap_key, self.decay_rate)
+
+    def isFull(self):
+        return self.curr_k == self.k # len(self._heap) >= self.k
+
+    def averageTopC(self):
+        ave = 0.
+        if self.curr_k > 0:
+            ave = torch.sum([it.norm() for it in self._heap]) / float(self.curr_k)
+        return ave
+
+    def getMin(self):
+        """
+        Get smallest gradient
+        :return: The smallest gradient
+        """
+        return self._heap[self.smallest]
+
+    def getMax(self):
+        "Returns the largest gradient"
+        return self._heap[torch.argmax(self._heap_key)]
+
+    def __getitem__(self, key):
+        return self._heap[self._heap_key==key]
+
+    def __len__(self):
+        return self.curr_k
+
+    def setdefault(self, key, val):
+        if key not in self:
+            self[key] = val
+            return val
+        return self[key]
+
+    def step(self):
+        for item in self._heap: item.step()
+
+    def epoch(self):
+        ages = []
+        for item in self._heap:
+            ages.append(item.epoch_age)
+            item.resetEpoch()
+        return ages
+
+
 
 
 class PriorityDict(dict):
@@ -32,10 +137,13 @@ class PriorityDict(dict):
         self._heap = [HeapItem(k, v) for k, v in self.items()]
         self._rebuild_heap()
 
+    def getNorms(self):
+        return [item.p for item in self._heap]
+
     def size(self):
         return len(self._heap)
 
-    def set_hyper(self, decay_rate=0.5, K=5):
+    def setHyper(self, decay_rate=0.5, K=5):
         self.k = K
         self.decay_rate = decay_rate
 
@@ -51,10 +159,10 @@ class PriorityDict(dict):
         self._heap = [it for it in self._heap if it.p >= 0.0]
         if len(self._heap) > 0:
             heapify(self._heap)
-            if not self.is_empty() and self.is_full():
+            if not self.isEmpty() and self.isFull():
                 self._reorder()
 
-    def is_empty(self):
+    def isEmpty(self):
         if len(self._heap) == 0:
             return True
         return False
@@ -62,18 +170,18 @@ class PriorityDict(dict):
     def decay(self):
         self._heap = [HeapItem(self.decay_rate * it.p, it.t) for it in self._heap]
 
-    def is_full(self):
+    def isFull(self):
         if len(self._heap) < self.k:
             return False
         return True
 
-    def average_topC(self):
+    def averageTopC(self):
         ave = 0.
         if len(self._heap) > 0:
             ave = sum([it.t.norm() for it in self._heap]) / float(len(self._heap))
         return ave
 
-    def poke_smallest(self):
+    def pokeSmallest(self):
         """Return the lowest priority.
 
         Raises IndexError if the object is empty.
@@ -82,7 +190,7 @@ class PriorityDict(dict):
         it = self._heap[0]
         return it.p
 
-    def gradmean(self):
+    def gradMean(self):
         """Return the sum of top k gradients
         """
 
@@ -93,7 +201,7 @@ class PriorityDict(dict):
             cnt += 1.
         return mean.div_(cnt)
 
-    def gradsum(self):
+    def gradSum(self):
         """Return the sum of top k gradients
         """
 
@@ -136,4 +244,4 @@ class PriorityDict(dict):
         """
 
         while self:
-            yield self.pop_smallest()
+            yield self.popSmallest()
